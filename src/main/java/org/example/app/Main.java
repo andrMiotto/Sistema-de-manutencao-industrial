36c8f29200dd4abfa6c85c9f30882fc678ef8c0e
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2025-09-17 21:12:24 -0300

    feat:cria-base

---
+++ src/main/java/org/example/app/Main.java
@@ -0,0 +1,15 @@
+package org.example.app;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+         Scanner SC = new Scanner(System.in);
+
+
+
+
+    }
+}
